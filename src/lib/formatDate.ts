2c9e45326bcbf8a0ba23244f81fab1d5d9d95ab3
Mhretab Kidane <mhretab@users.noreply.github.com>
2022-08-28 21:43:15 +0200

    feat: updated readme and created init script

--- src/lib/formatDate.ts
+++
@@ -1,6 +0,0 @@
-const formatDate = (dateObj: Date): string =>
-  `${dateObj.getFullYear()}-${('0' + (dateObj.getMonth() + 1)).slice(-2)}-${(
-    '0' + dateObj.getDate()
-  ).slice(-2)}`;
-
-export default formatDate;
--- src/lib/isDevLogEnabled.ts
+++
@@ -1,3 +0,0 @@
-const isDevLogEnabled = (): boolean =>
-  process.env.NODE_ENV === 'development' && process.env.DEV_LOG === 'true';
-export default isDevLogEnabled;
