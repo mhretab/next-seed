44d522ee6600ca22799b00ed254fee8c70667c7a
Mhretab Kidane <mhretab@users.noreply.github.com>
2022-08-29 18:28:59 +0200

    feat: use tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,33 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => (
-  <div className={styles.container}>
+  <div className="flex min-h-screen flex-col items-center justify-center py-2">
     <Head>
       <title>Next.js starter boilerplate</title>
       <meta content="Next.js starter boilerplate in Typescript" name="description" />
       <link href="/favicon.ico" rel="icon" />
     </Head>
 
-    <main className={styles.main}>
-      <h1 className={styles.title}>
-        Welcome to <a href="https://nextjs.org">Next.js!</a> starter boilerplate.
+    <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+      <h1 className="text-6xl font-bold">
+        Welcome to{' '}
+        <a className="text-blue-600" href="https://nextjs.org">
+          Next.js!
+        </a>{' '}
+        starter boilerplate.
       </h1>
     </main>
 
-    <footer className={styles.footer}>
+    <footer className="flex h-24 w-full items-center justify-center border-t">
       <a
+        className="flex items-center justify-center gap-2"
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         rel="noopener noreferrer"
         target="_blank"
       >
-        Powered by{' '}
-        <span className={styles.logo}>
-          <Image alt="Vercel Logo" height={16} src="/vercel.svg" width={72} />
-        </span>
+        Powered by <Image alt="Vercel Logo" height={16} src="/vercel.svg" width={72} />
       </a>
     </footer>
   </div>
