d569ad1dbc10847d48e2bf2abf1969fc9cef87ca
Mhretab Kidane <mhretab@users.noreply.github.com>
2022-08-30 19:14:59 +0200

    fix: improved configure script

--- meeseeks.sh
+++ meeseeks.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+
 set -- $(locale LC_MESSAGES)
 yesexpr="$1"; noexpr="$2"; yesword="$3"; noword="$4"
 
 DASH=----------------------------------------------------------------------------------
-
 echo $DASH
 
+# setup a package
 configure () {
   while true; do
     read -p "Would you like to $1 (Y/n): " yn
     if [[ "$yn" =~ $yesexpr ]]; 
     then
       echo "🔳 $2"
-      git merge -X ours $3 --no-edit
+      git merge -X ours "origin/$3" --no-edit
       pnpm install
       git add .
       git commit --amend --no-edit
@@ -35,12 +36,14 @@ configure "install react-aria and react-stately: " "Setting up react-aria and re
 configure "add tailwindcss: " "Setting up tailwindcss" "tailwindcss"
 configure "add firebase: " "Setting up firebase" "firebase"
 
-git branch | grep -v "main" | xargs git branch -D
-
+# Delete configure script from package.json
 sed '6d' package.json >> new-package.json
 rm package.json
 mv new-package.json package.json
+
+# Delete self 
+rm meeseeks.sh
+
+# Commit both deletions with no edit
 git add .
 git commit --amend --no-edit
-
-rm meeseeks.sh
