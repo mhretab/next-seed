901b8ef3d8ae59986ce9909a0330141f044655ca
Mhretab Kidane <mhretab@users.noreply.github.com>
2022-09-04 21:53:56 +0200

    feat: git setup and functions

---
+++ src/git.js
@@ -0,0 +1,28 @@
+import { simpleGit } from 'simple-git';
+import { createSpinner } from 'nanospinner';
+
+export const initGit = async (appName) => {
+  const spinner = createSpinner(`Initializing`).start();
+  const git = simpleGit({
+    baseDir: process.cwd(),
+    binary: 'git',
+    maxConcurrentProcesses: 8,
+    trimmed: false,
+  });
+
+  await git.clone('git@github.com:mhretab/next-seed-template.git', appName);
+  await git.cwd(`${process.cwd()}/${appName}`);
+  await git.fetch();
+  spinner.success({ text: `Initializing complete` });
+  return git;
+};
+
+export const merge = async (git, branchName) => {
+  const spinner = createSpinner(`Setting up ${branchName}`).start();
+  await git.fetch();
+  await git.merge([`origin/${branchName}`, '-Xours', '--no-edit']);
+  await git.add(['.']);
+  const result = await git.commit(['--amend', '--no-edit']);
+  spinner.success({ text: `[Done] - Setting up ${branchName}` });
+  return result;
+};
